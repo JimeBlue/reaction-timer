da6ea2e071093a93f8056a77692aa62a062a3ef5
Jimena <jimeblue69@gmail.com>
2022-08-16 19:34:32 +0200

    Keep track whether user is playing and set delay

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
+  <!-- on click call the function start -->
   <button @click="start">play</button>
 </template>
 
@@ -7,6 +8,22 @@
 export default {
   name: 'App',
   components: {},
+  data() {
+    return {
+      // By default is false, it changes to true when the play btn is clicked
+      isPlaying: false,
+      //The amount of time there´s going to be before Block appears on the screen
+      delay: null,
+    };
+  },
+  methods: {
+    // On click this function updates delay to something between 2 and 4 seconds and sets isPlaying to true
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      console.log(this.delay);
+    },
+  },
 };
 </script>
 
