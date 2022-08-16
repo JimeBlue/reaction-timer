68ec7e401e821257f3b98523328637197ab9329f
Jimena <jimeblue69@gmail.com>
2022-08-16 20:08:05 +0200

    Add template and style to the block component

---
+++ src/components/Block.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="block">Click me</div>
+</template>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
